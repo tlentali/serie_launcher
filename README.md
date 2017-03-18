b715ac6fc69a04db2d93544378060a149b625805
roibos <thomas.lentali@gmail.com>
2017-03-18 23:59:38 +0100

    first commit with code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # serie_launcher
+
+Permit you not to be bothered by the so annoying : "on en était où deja?" when you get back to your serie.
+Launch the program each time you want to watch an episode.
+
+![Project visual](https://i.kinja-img.com/gawker-media/image/upload/s--AvXFLgAY--/c_scale,fl_progressive,q_80,w_800/hptu45lgngxq2tb7bet5.jpg)
+
+
+## Launcher example
+
+If you want to watch Friends :  
+`./friends.sh`
---
+++ update_doc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys
+import pandas as pd
+import numpy as np
+import webbrowser
+
+df = pd.read_csv('saison_episode.csv')
+url = "http://onwatchseries.to/episode/friends_s" + str(df["saison"].iloc[0]) \
+      + "_e" + str(df["episode"].iloc[0]) + ".html"
+
+# open in a new tab, if possible
+new = 2
+
+# open a public URL, in this case, the webbrowser docs
+webbrowser.open(url, new=new)
+
+df["episode"].iloc[0] += 1
+if df["episode"].iloc[0] > 25:
+    df["episode"].iloc[0] = 1
+    df["saison"].iloc[0] += 1
+
+df.to_csv('saison_episode.csv')
